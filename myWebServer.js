279c5c81788a28b1b9ab02e3f850208a216cff24
Tadiyos Mesfin <taddmesfin@gmail.com>
2025-01-01 20:44:25 +0300

    update node practice

--- myWebServer.js
+++ myWebServer.js
@@ -2,7 +2,7 @@ const http = require("http");
 const random = require("./randomNumber");
 const server = http.createServer((req, res) => {
     const randomNumber = random();
-    console.log(`Generated Random Number: ${randomNumber}`);
+
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(`Random Number: ${randomNumber}`);
 });
---
+++ myWebServer2.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const mime = require("mime-types");
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url;
+  if (filePath === "/about") {
+    filePath = "/about.html";
+  } else if (filePath === "/") {
+    filePath = "/index.html";
+  }
+
+  let requestedFile = __dirname + "/static/appleClone-with-HTML-CSS/" + filePath;
+
+
+  fs.readFile(requestedFile, (err, content) => {
+    if (err) {
+      const notFoundFile =
+        __dirname + "/appleClone-with-HTML-CSS/notFound.html";
+      fs.readFile(notFoundFile, (err, notFoundContent) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("404 Not Found");
+        } else {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(notFoundContent);
+        }
+      });
+    } else {
+      const fileType = mime.lookup(filePath) || "application/octet-stream";
+      res.writeHead(200, { "Content-Type": fileType });
+      res.end(content);
+    }
+  });
+});
+server.listen(1235, () => {
+  console.log("Server running on http://localhost:1235");
+});
